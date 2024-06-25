0448704b76a65ea0397a90793cf4e4ee0f443d66
DadMed <mohammed.dad.ens@gmail.com>
2024-06-25 19:09:22 +0100

    update

--- inc/maze.h
+++ inc/maze.h
@@ -1,12 +1,11 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-#define MAP_WIDTH 24
-#define MAP_HEIGHT 24
+#define MAZE_MAP_WIDTH 24
+#define MAZE_MAP_HEIGHT 24
 
 extern const int MAP_WIDTH;
 extern const int MAP_HEIGHT;
-
 
 
 // Function prototypes
